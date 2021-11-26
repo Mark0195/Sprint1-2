c1e831912c32bf72eb263cb8704ea2eb35f7f8d8
Mark0195 <mark.myles@keyin.com>
2021-11-26 10:29:51 -0330

    Added dependencies, updated client

--- src/main/java/httpRequest/MainHttpClient.java
+++ src/main/java/httpRequest/MainHttpClient.java
@@ -1,5 +1,6 @@
 package httpRequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -15,44 +16,73 @@ public class MainHttpClient {
     private static int input;
     public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
         while (true) {
-            System.out.println("Press 1 to add, Press 2 to get , Press 3 to update , " +
-                    "Press 4 to delete , Press 0 to exit");
+            System.out.println("Press 1 to Add, Press 2 to Get, Press 3 to Update, " +
+                    "Press 4 to Delete, Press 0 to Cancel");
             scanner = new Scanner(System.in);
             input = scanner.nextInt();
 
             switch(input){
                 case 0:
-                    System.out.println("exiting!");
+                    System.out.println("Exiting....");
                     System.exit(0);
                 case 1:
-                    //httpPostPerson();
+                    httpPostMembership();
                     break;
                 case 2:
-                    httpGetPerson();
+                    httpGetMembership();
                     break;
                 case 3:
-                    //httpPutPerson();
+                    httpPutMembership();
                     break;
                 case 4:
-                    httpDeletePerson();
+                    httpDeleteMembership();
                     break;
             }
         }
     }
+    public static void httpPostMembership() throws IOException, InterruptedException {
+        Map<Object, Object> people = new HashMap<>();
+        people.put("firstName", "Lenny");
+        people.put("lastName", "Leonard");
+        people.put("address", "123 Street");
+        people.put("email", "LennyL@hotmail.com");
+        people.put("phoneNumber", 1234567);
+        people.put("startDate", "10/24/2010");
+        people.put("duration", "10/24/2011");
+        people.put("type", "normal");
+        people.put("currentId", "8");
+        people.put("pastId", "5");
+        people.put("upcomingId", "4");
 
-    public static void httpGetPerson() throws IOException, InterruptedException {
+        ObjectMapper posted = new ObjectMapper();
+        String requestBody = posted
+                .writeValueAsString(people);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/person"))
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("Posted Membership : " + response.body());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void httpGetMembership() throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         String peoples;
-        //System.out.println("Enter first name: ");
-        System.out.println("Enter last name: ");
-        //System.out.println("Enter address: ");
-        //System.out.println("Enter email: ");
-        //System.out.println("Enter phone number: ");
+        System.out.println("Please enter the id number of the person you are looking for: ");
         peoples = scanner.next();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/person/search/findByLastName?lastName="))
+                .uri(URI.create("http://localhost:8080/membership"))
+                .GET()
                 .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -65,27 +95,59 @@ public class MainHttpClient {
         }
 
     }
-    public static void httpPostPerson() throws IOException, InterruptedException {
-        Map<Object, Object> person = new HashMap<>();
-        person.put("firstName", "");
-        person.put("lastName", "");
-        person.put("address", "");
-        person.put("email", "");
-        person.put("phoneNum", "");
+
+    public static void httpPutMembership() throws IOException, InterruptedException {
+        Map<Object, Object> people = new HashMap<>();
+        people.put("firstName", "Pete");
+        people.put("lastName", "Peterson");
+        people.put("address", "321 Street");
+        people.put("email", "Peteyp@gmail.com");
+        people.put("phoneNumber", "3214567");
+        people.put("startDate", "10/11/2009");
+        people.put("duration", "10/11/2012");
+        people.put("type", "trial");
+        people.put("currentId", "4");
+        people.put("pastId", "2");
+        people.put("upcomingId", "3");
+
+        ObjectMapper posted = new ObjectMapper();
+        String requestBody = posted
+                .writeValueAsString(people);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/membership"))
+                .PUT(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("Updated Membership : " + response.body());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void httpDeletePerson() throws IOException, InterruptedException {
+
+
+    public static void httpDeleteMembership() throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
         String deleted;
-        System.out.println("Type Id of the person to delete: ");
+        System.out.println("Type Id of the membership to delete: ");
         deleted = scanner.next();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/person/search/"))
+                .uri(URI.create("http://localhost:8080/membership/search/"))
                 .DELETE()
                 .build();
         HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String responseBody = response.body();
-        System.out.println("File Deleted : " + responseBody);
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                System.out.println("Deleted Membership : " + response.body());
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
